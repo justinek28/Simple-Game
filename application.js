28b799c064124baabb98d2426da06d8406732935
justinek28 <henderson.justine@gmail.com>
2015-08-27 19:27:30 -0700

    update

--- application.js
+++ application.js
@@ -1,9 +1,43 @@
-$(document).ready(function() {
+$(document).ready(function(){
+  var randomNumber = Math.floor(Math.random()*9);
+  var turnsRemaining = 3;
+  var $counter = $("#counter");
+  var gameStates = {
+    'on': 'Treasure Hunt',
+    'won': 'You won!',
+    'lost': 'Game Over'
+  };
+  var currentGameState = 'on';
 
+  $counter.text(turnsRemaining);
+  var isGameStillOn = function() {
+    return currentGameState === 'on';
+  }
+  var changeGameState = function(newState) {
+    currentGameState = newState;
+    $("#headerMessage").text(gameStates[currentGameState]);
+  };
 
-$('.img-btn.active').live('click',function(){
-    $('.img-btn').toggleClass('active');
-    $('#test').slideToggle();
+  var handleClick = function(event){
+    if (isGameStillOn()) {
+      event.preventDefault();
+      decrementTurnsRemaining();
+      var $link = $(this);
+      var correctAnswer = $link.data("id") === randomNumber;
+      $link.addClass(correctAnswer ? "winner" : "loser");
+      if (correctAnswer) {
+        changeGameState('won');
+        return;
+      }
+    }
+    if (turnsRemaining === 0) {
+      changeGameState('lost');
+    }
+  };
+
+  $(".btn").on("click", handleClick);
+
+  var decrementTurnsRemaining = function() {
+    $counter.text(--turnsRemaining);
+  };
 });
-
-}); 
